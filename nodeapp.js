15a25a6d52a5b8a3c30611cc86d5684b5c3e58d2
Xen Abano <xeniamae.abano@gmail.com>
2021-10-28 20:37:08 +0800

    heroku web app mini capstone

---
+++ nodeapp.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const PORT = process.env.PORT || 3000 ;
+const app = express();
+const cors = require('cors');
+
+let result = ''
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(cors());
+
+
+app.post('/', (req, res) => {
+	console.log(req.body)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/result', (req, res) => {
+  res.send(result)
+})
+
+app.listen(PORT, () => {
+  console.log(`App listening at:${PORT}`)
+})
